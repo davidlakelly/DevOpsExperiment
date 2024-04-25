a1d77723a447dace70bad4c12feb7397531e6d01
Sean Elliott <sean.elliott7@hotmail.com>
2024-04-25 20:50:55 +0100

    login test

--- app/hello.py
+++ app/hello.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -12,5 +12,17 @@ def render_html():
     users = ["David", "John", "Paul", "George"]
     return render_template('sample.html', name="David",users=users )
 
+# This is the route fo the login page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect('/')
+    return render_template('loginpage.html', error=error)
+
 if __name__ == '__main__':
     app.run()
+
