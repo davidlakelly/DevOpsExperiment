e48cca05af4e26140477e867d37051a8b5b0efbc
Sean Elliott <sean.elliott7@hotmail.com>
2024-04-27 13:02:54 +0100

    datapage test

--- app/hello.py
+++ app/hello.py
@@ -2,7 +2,23 @@ from flask import Flask
 from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
-
+switchList = [
+        {
+        "name": "C9200",
+        "serial": "ABCD00010",
+        "ip": "192.168.1.1"     
+    },
+    {
+        "name": "C9300",
+        "serial": "ABCD00011",
+        "ip": "172.168.44.2"  
+    },
+        {
+        "name": "C9400",
+        "serial": "ABCD00012",
+        "ip": "172.33.44.2"    
+    }
+]
 @app.route('/')
 def hello():
     return 'login sucess'
@@ -12,6 +28,13 @@ def render_html():
     users = ["David", "John", "Paul", "George"]
     return render_template('sample.html', name="David",users=users )
 
+
+@app.route('/datapage')
+def render_data():
+    return render_template('datapage.html', switches=switchList )
+
+
+
 # This is the route fo the login page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -20,7 +43,7 @@ def login():
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
         else:
-            return redirect('/')
+            return redirect('/datapage')
     return render_template('loginpage.html', error=error)
 
 if __name__ == '__main__':
