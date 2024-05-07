acbbee3c1b5c9bbb58ffdf5f946e3f9717526b31
Sean Elliott <sean.elliott7@hotmail.com>
2024-05-07 21:40:09 +0100

    session end - 20:39 07/05/2024

--- app/hello.py
+++ app/hello.py
@@ -4,31 +4,42 @@ import db_query
 import os
 import sys
 app = Flask(__name__)
-authflag = 0
+authflag = 1
 #temp switch list pending methods
 switchList = [
         {
         "indic_de": "C9200",
-        "geo": "ABCD00010",
-        "TIME_PERIOD": "192.168.1.1" ,    
-         "OBS_VALUE": "ABCD00010",
-        "OBS_FLAG": "192.168.1.1" 
+        "geo": "IRE",
+        "TIME_PERIOD": "2001" ,    
+         "OBS_VALUE": "1000",
+        "OBS_FLAG": "A" 
     },
     {
         "indic_de": "C9200",
-        "geo": "ABCD00010",
-        "TIME_PERIOD": "192.168.1.1" ,    
-         "OBS_VALUE": "ABCD00010",
-        "OBS_FLAG": "192.168.1.1" 
+        "geo": "IRE",
+        "TIME_PERIOD": "2002" ,    
+         "OBS_VALUE": "1500",
+        "OBS_FLAG": "A" 
     },
         {
         "indic_de": "C9200",
-        "geo": "ABCD00010",
-        "TIME_PERIOD": "192.168.1.1" ,    
-         "OBS_VALUE": "ABCD00010",
-        "OBS_FLAG": "192.168.1.1" 
+        "geo": "IRE",
+        "TIME_PERIOD": "2003" ,    
+         "OBS_VALUE": "1800",
+        "OBS_FLAG": "A" 
     }
 ]
+
+labels = [
+        '2001',
+        '2002',
+        '2003',
+        '2004',
+        '2005',
+        '2006',
+    ]
+ 
+data = [1000, 1200, 1285, 1324, 1589, 1870, 10000]
 
 # get data from api and return it in a route
 
@@ -76,15 +87,22 @@ def render_data():
     else: #attempting to access page berfore login
         return render_template('loginpage.html', error = 'Unauthroised Access Attempt')
 
+
 @app.route('/graphpage')
-def render_graph():
+def render_graph_demo():
+    temp_labels = [x['TIME_PERIOD'] for x in switchList]
+    temp_data = [y['OBS_VALUE'] for y in switchList]
+
     return render_template(
-        template_name_or_list='chartjs-example.html',
-        switches=switchList,
-    )
+    template_name_or_list='graphpage.html',
+    data=temp_data,
+    labels=temp_labels)
+
+
+
 @app.route('/download')
 def downloadFile ():
-    path = "static/test.txt"
+    path = "static/test.csv"
     #print working directory
     print(os.getcwd(), file=sys.stdout)
     #list directory
@@ -95,18 +113,6 @@ def downloadFile ():
     return send_file(path, as_attachment=True)
 
 
-# This is the route fo the login page
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-            authflag = 0
-        else:
-            authflag = 1
-            return redirect('/datapage')
-    return render_template('loginpage.html', error=error)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
